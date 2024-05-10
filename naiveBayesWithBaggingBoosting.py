07a2dcfaac1767aeb4b554dfdebb25f73613f751
canokcan <133148243+canokcan@users.noreply.github.com>
2024-05-10 09:11:56 +0300

    recall and harmonic mean implemented, see extended desc.
    I got same accuracy and recall values for over 30 tests, when I researched this, this can be happening due to dataset's attributes. Additionally, it is stated that this is not quite uncommon.

--- naiveBayesWithBaggingBoosting.py
+++ naiveBayesWithBaggingBoosting.py
@@ -1,6 +1,7 @@
 import time
 from sklearn.naive_bayes import GaussianNB
-from sklearn.metrics import accuracy_score, f1_score
+from sklearn.metrics import accuracy_score, f1_score, recall_score
+from statistics import harmonic_mean 
 from sklearn.ensemble import BaggingClassifier, AdaBoostClassifier
 from sklearn.model_selection import train_test_split, cross_val_score
 from sklearn.feature_extraction.text import CountVectorizer
@@ -57,8 +58,10 @@ bagging_naive_bayes.fit(X_train, y_train)
 y_pred_bagging = bagging_naive_bayes.predict(X_test)
 accuracy_bagging = accuracy_score(y_test, y_pred_bagging) * 100
 f1_bagging = f1_score(y_test, y_pred_bagging, average='weighted') * 100 #F1 Scores added here
+recall_bagging = recall_score(y_test, y_pred_bagging, average='weighted')*100 #recall values here
 print(f'Accuracy on test set (Bagging Naive Bayes): {accuracy_bagging:.2f}%')
 print(f'F1 Score on test set (Bagging Naive Bayes): {f1_bagging:.2f}%')
+print(f'Recall result (Bagging Naive Bayes): {recall_bagging:.2f}%')
 
 # boosting
 boosting_naive_bayes = AdaBoostClassifier(GaussianNB(), n_estimators=10, algorithm='SAMME', random_state=42) #"FutureWarning: The SAMME.R algorithm (the default) is deprecated and will be removed in 1.6.
@@ -67,9 +70,21 @@ boosting_naive_bayes.fit(X_train, y_train)
 y_pred_boosting = boosting_naive_bayes.predict(X_test)
 accuracy_boosting = accuracy_score(y_test, y_pred_boosting) * 100
 f1_boosting = f1_score(y_test, y_pred_boosting, average='weighted') * 100 #F1 Scores added here
+recall_boosting = recall_score(y_test, y_pred_boosting, average='weighted')*100  #recall here
 print(f'Accuracy on test set (Boosting Naive Bayes): {accuracy_boosting:.2f}%')
 print(f'F1 Score on test set (Boosting Naive Bayes): {f1_boosting:.2f}%')
+print(f'Recall result (Boosting Naive Bayes): {recall_boosting:.2f}%')
 
+#!!!NOTE!!! I AM GETTING SIMILAR RECALL AND ACCURACY RESULTS FOR BOTH BAGGING AND BOOSTING, POSSIBLY DUE TO DATASET AT HAND BEING BALANCED, 
+#THIS IS NOT IMPOSSIBLE FROM WHAT I CAN SEE RESEARCHING!!!
+
+#harmonic mean calculated for bagging here, I included accuracy, f1, recall values for now
+harmonic_mean_bagging = harmonic_mean([accuracy_bagging, f1_bagging, recall_bagging])
+print(f'Harmonic Mean of Accuracy, F1 Score, and Recall (Bagging): {harmonic_mean_bagging:.2f}%')
+
+#harmonic mean calculated for boosting here, I included accuracy, f1, recall values for now 
+harmonic_mean_boosting = harmonic_mean([accuracy_boosting, f1_boosting, recall_boosting])
+print(f'Harmonic Mean of Accuracy, F1 Score, and Recall (Boosting): {harmonic_mean_boosting:.2f}%')
 
 # cross-validation with bagging
 bagging_cv_scores = cross_val_score(bagging_naive_bayes, X, y, cv=5)
